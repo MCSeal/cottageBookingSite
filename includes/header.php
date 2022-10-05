9e93c61347238eccacbe33c3b2172ed127d5329e
MCSeal <mzeal93@gmail.com>
2022-10-05 16:45:11 -0400

    first

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Attendace - <?php echo $title ?></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Cottage Booking</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="viewrecords.php">View Attendees</a>
+              </li>
+
+            </ul>
+          </div>
+      </nav>
+    <div class="container">
+
+    <br><br>
---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+$title = "Index";
+require_once "includes/header.php";
+
+
+
+?>
+
+    
+    <h1 class="text-center">Hello!</h1>
+
+    
+
+<?php require_once "includes/footer.php";
+?>
